d3b1a14d5c0f94896b45d7c05ec81457b68e17ae
Roja Bugade <69809763+BRojaGitHub@users.noreply.github.com>
2021-10-11 11:00:47 -0400

    Updated Controller

--- src/main/java/com/roja/demo/controller/AlienController.java
+++ src/main/java/com/roja/demo/controller/AlienController.java
@@ -1,26 +1,15 @@
 package com.roja.demo.controller;
 
-//import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.roja.demo.dao.AlienRepo;
 import com.roja.demo.model.Alien;
-
-//DB
-//url : http://localhost:8080/h2-console/
-//select * from alien
 
 @Controller
 public class AlienController 
@@ -47,10 +36,7 @@ public class AlienController
 		ModelAndView mv = new ModelAndView("showAlien.jsp");
 		Alien alien = repo.findById(aid).orElse(new Alien());
 		mv.addObject(alien);
-		return mv;
-		/*System.out.println(repo.findByTech("Java"));
-		System.out.println(repo.findByAidGreaterThan(102));
-		System.out.println(repo.findebySorted("Java"));*/
+		return mv; 
 	}
 	
 	@RequestMapping("/deleteAlien")
@@ -66,7 +52,7 @@ public class AlienController
 	public String saveOrUpdateAlien(Alien alienU)
 	{
 		repo.save(alienU);
-		return "editAlienJump.jsp";
+		return "editjump.jsp";
 	}
 }
 
